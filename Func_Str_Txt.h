c425e2f9201b68d39e5b1be757d0271fc10eb3e8
Artemelion <artyommorar@gmail.com>
2025-04-30 09:29:39 +0300

    Update Func_Str_Txt.h
    download header correctly

--- Func_Str_Txt.h
+++ Func_Str_Txt.h
@@ -1,1 +1,11 @@
-#pragma once
+int myStrLen(const char* str);
+int myStrCmp(const char* str1, const char* str2);
+void myStrCpy(char* dest, const char* source);
+void myStrCat(char*& dest, const char* source);
+char* myStrStr(char* haystack, const char* needle);
+
+
+
+int countOccurrences(const char* text, char symbol);
+int countDotsAndCommas(const char* text);
+int countSentences(const char* text);
